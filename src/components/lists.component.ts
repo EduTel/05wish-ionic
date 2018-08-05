8b95742ac2dad426b622c89b159bcf613661fb4c
EduardoTellez <15623704+EduTel@users.noreply.github.com>
2018-08-04 22:11:50 -0500

    constructor y ItemSliding.close()

--- src/components/lists.component.ts
+++ src/components/lists.component.ts
@@ -5,6 +5,9 @@ import { AddPage } from './../pages/add/add.component';
 
 import { Component, OnInit, Input } from '@angular/core';
 
+import { AlertController,ItemSliding } from 'ionic-angular';
+
+
 @Component({
   selector: 'app-listas',
   templateUrl: './lists.component.html',
@@ -12,7 +15,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListasComponent implements OnInit {
   @Input() c_finish:boolean;
-  constructor(public _WishService: WishService, private navCtr: NavController){
+  constructor(public _WishService: WishService, private navCtr: NavController,public _AlertController:AlertController){
 
   }
   ngOnInit( ): void {
@@ -24,5 +27,37 @@ export class ListasComponent implements OnInit {
       titulo: _list.titulo,
       lista: _list
     });
+  }
+  EditNameList(_itemList: List,_ItemSliding:ItemSliding  ) {
+    _ItemSliding.close();
+    const prompt = this._AlertController.create({
+      title: 'Editar',
+      message: "Edit the name list",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'edit',
+          value: _itemList.titulo
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            if(data.title.length !==0 ){
+              _itemList.titulo=data.title;
+              this._WishService.saveStorage();
+            }
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 }
